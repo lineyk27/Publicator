6bc95e8731bfdab9202ee8624cee8fe512b3e826
lineyk27 <lineyk27gg@gmail.com>
2020-01-19 23:19:12 +0200

    Added injection infrastructure services

--- src/Publicator.Infrastructure/InfrastructureServices.cs
+++ src/Publicator.Infrastructure/InfrastructureServices.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Publicator.Infrastructure.Interfaces;
 
 namespace Publicator.Infrastructure
 {
@@ -6,7 +9,16 @@ namespace Publicator.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
         {
-
+            var conn = "DefaultConnection";
+            services.AddScoped<IUnitOfWork>(provider => {
+                var Configuration = provider.GetService<IConfiguration>();
+                return new UnitOfWork(Configuration.GetConnectionString(conn));
+            });
+            services.AddDbContext<PublicatorDbContext>(options =>
+            {
+                var Configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+                options.UseSqlServer(Configuration.GetConnectionString(conn));
+            });
             return services;
         }
     }
