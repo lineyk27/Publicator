7172aec7e51cd0cf139382845bdd6ade2365b828
lineyk27 <lineyk27gg@gmail.com>
2020-01-20 20:56:23 +0200

    Added contracts for services

--- src/Publicator.ApplicationCore/Interfaces/IPostService.cs
+++ src/Publicator.ApplicationCore/Interfaces/IPostService.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Publicator.Infrastructure.Entities;
 
 namespace Publicator.ApplicationCore.Interfaces
 {
     interface IPostService
     {
+        public Task<IEnumerable<Post>> GetAllAsync();
+        public Task<IEnumerable<Post>> GetByTagAsync(string tag);
+        public Task<Post> GetByIdAsync(Guid postid);
+        public Task<IEnumerable<Post>> GetBySubscriptionAsync(Guid userid);
+        public Task<IEnumerable<Post>> GetByCreatorAsync(User creatoruser);
+        public Task<IEnumerable<Post>> GetVotedByCreatorAsync(User creatorvoteuser);
     }
 }
--- src/Publicator.ApplicationCore/Interfaces/IUserService.cs
+++ src/Publicator.ApplicationCore/Interfaces/IUserService.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Publicator.Infrastructure.Entities;
 
 namespace Publicator.ApplicationCore.Interfaces
 {
     interface IUserService
     {
+        public Task<User> GetCurrentUserAsync();
+        public Task<User> GetByUsernameAsync(string username);
+        public Task<User> GetByIdAsync(Guid id);
+        public Task<IEnumerable<User>> GetByRoleAsync(Role role);
+        public Task<IEnumerable<User>> GetByStateAsync(State role);
+        public Task<IEnumerable<User>> GetBySearchAsync(string query);
+        public Task<IEnumerable<User>> GetSubscriptionsAsync(User subscriberuser);
+        public Task<IEnumerable<User>> GetSubscribersAsync(User subscriptionuser);
     }
 }
