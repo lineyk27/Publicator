11ad099eb49249c703ce3bbadfeee5479d685ac7
lineyk27 <lineyk27gg@gmail.com>
2020-01-21 21:14:30 +0200

    Injected app core services in startup

--- src/Publicator.Presentation/Startup.cs
+++ src/Publicator.Presentation/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Publicator.Infrastructure;
+using Publicator.ApplicationCore;
 
 namespace Publicator.Presentation
 {
@@ -21,6 +22,7 @@ namespace Publicator.Presentation
                 );
 
             services.AddInfrastructureServices();
+            services.AddApplicationCoreServices();
             services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
