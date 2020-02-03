ebf3581f3ed1cb3f732b7d9df5418c2ccbb7ac2e
lineyk27 <lineyk27gg@gmail.com>
2020-02-03 16:05:48 +0200

    Removed client app

--- src/Publicator.Presentation/ClientApp/src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import loginReducer from "./loginReducer";
-import {combineReducers} from "redux";
-
-// TODO add more reducers
-export default combineReducers({
-    loginReducer
-})
--- src/Publicator.Presentation/ClientApp/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-
-export default createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-);
