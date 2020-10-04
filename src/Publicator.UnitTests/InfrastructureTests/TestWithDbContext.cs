9d5cfa4cee03a1ca4c5b235d41e4c321944c1f73
lineyk27 <lineyk27gg@gmail.com>
2020-10-04 21:10:53 +0300

    Added starter project for tests

---
+++ src/Publicator.UnitTests/InfrastructureTests/TestWithDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Publicator.Infrastructure;
+
+namespace Publicator.UnitTests.InfrastructureTests
+{
+    class TestWithDbContext : IDisposable
+    {
+        protected readonly PublicatorDbContext dbContext;
+        public TestWithDbContext()
+        {
+            string connString = "Server=DESKTOP-I4GI6P8\\SQLEXPRESS;" +
+                "Database=PublicatorTest;" +
+                "Trusted_Connection=True;" +
+                "MultipleActiveResultSets=true";
+
+            var optionsBuilder = new DbContextOptionsBuilder<PublicatorDbContext>().UseSqlServer(connString);
+
+            dbContext = new PublicatorDbContext(optionsBuilder.Options);
+            dbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+    }
+}
